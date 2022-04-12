b9a9c4dacdd1da26fd2806f049cf52d1fe1ae3d6
anna-cross <annakh@meroxa.io>
2022-04-12 16:38:44 -0400

    fix merge

--- python-sample-data-app/main.py
+++ python-sample-data-app/main.py
@@ -7,19 +7,19 @@ from turbine import Turbine
 from turbine.runtime import Record
 import pdb
 
+
 def anonymize(records: t.List[Record]) -> t.List[Record]:
     updated = []
     for record in records:
         try:
             value_to_update = record.value
             hashed_email = hashlib.sha256(
-                value_to_update['payload']['after']['email'].encode()).hexdigest()
-            value_to_update['payload']['after']['email'] = hashed_email
+                value_to_update["payload"]["after"]["email"].encode()
+            ).hexdigest()
+            value_to_update["payload"]["after"]["email"] = hashed_email
             updated.append(
                 Record(
-                    key=record.key,
-                    value=value_to_update,
-                    timestamp=record.timestamp
+                    key=record.key, value=value_to_update, timestamp=record.timestamp
                 )
             )
         except Exception as e:
@@ -29,44 +29,9 @@ def anonymize(records: t.List[Record]) -> t.List[Record]:
 
 
 class App:
-
     @staticmethod
     async def run(turbine: Turbine):
-<<<<<<< Updated upstream
-        # Get remote resource
         try:
-            # Find resource 
-            source = await turbine.resources("source_name")
-            if isinstance(source, ChildProcessError):
-                raise ChildProcessError(source)
-
-            # Read from remote resource
-            records = await source.records("collection_name")
-            if isinstance(records, ChildProcessError):
-                raise ChildProcessError(records)
-
-            # Deploy function with source as input
-            anonymized = await turbine.process(records, anonymize)
-            if isinstance(anonymized, ChildProcessError):
-                raise ChildProcessError(anonymized)
-
-            # Get destination
-            destination_db = await turbine.resources("destination_name")
-            if isinstance(destination_db, ChildProcessError):
-                raise ChildProcessError(destination_db)
-
-            # Write results out
-            results = await destination_db.write(anonymized, "collection_name")
-            if isinstance(results, ChildProcessError):
-                raise ChildProcessError(results)
-
-        except ChildProcessError as cpe: 
-            print(cpe)
-        except Exception as e: 
-            print(e)
-        
-=======
-        try: 
             # Get remote resource
             source = await turbine.resources("source_name")
 
@@ -81,25 +46,17 @@ class App:
 
             # Write results out
             await destination_db.write(anonymized, "collection_name")
-            
+
         except ChildProcessError as cpe:
             print(cpe)
         except Exception as e:
             print(e)
->>>>>>> Stashed changes
 
 
 def main():
     curr = os.path.abspath(os.path.dirname(__file__))
 
-    asyncio.run(
-        App.run(
-            Turbine(
-                runtime="local",
-                path_to_data_app=curr
-            )
-        )
-    )
+    asyncio.run(App.run(Turbine(runtime="local", path_to_data_app=curr)))
 
 
 if __name__ == "__main__":
