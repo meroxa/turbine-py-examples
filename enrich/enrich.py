ea2d4fe875bf55b8fc14d31e0a4673c863e8aaca
Eric Cheatham <eric@meroxa.io>
2022-04-18 18:40:59 -0500

    Add enrich data app example

---
+++ enrich/enrich.py
@@ -0,0 +1,42 @@
+import os
+
+import requests
+
+
+class UserDetails:
+    def __init__(self, full_name, location, role, seniority, company):
+        self.full_name = full_name
+        self.location = location
+        self.role = role
+        self.seniority = seniority
+        self.company = company
+
+
+class EnrichmentError(Exception):
+    pass
+
+
+def enrich_user_email(email: str):
+    clearbit_key = os.getenv("CLEARBIT_API_KEY")
+
+    params = {"email": email}
+    headers = {'Authorization': f'Bearer {clearbit_key}'}
+    response = requests.get(
+        url='https://person-stream.clearbit.com/v2/combined/find',
+        params=params,
+        headers=headers)
+
+    try:
+        person = response.json().get('person')
+        company = response.json().get('company')
+
+        return UserDetails(
+            full_name=person['name']['fullName'],
+            location=person['location'],
+            role=person['employment']['role'],
+            seniority=person['employment']['seniority'],
+            company=company['name'],
+        )
+    except Exception as e:
+        raise EnrichmentError(f"Error fetching information: {e}")
+
---
+++ enrich/main.py
@@ -0,0 +1,49 @@
+import logging
+import typing as t
+
+from turbine.runtime import Record, Runtime
+
+from enrich import enrich_user_email
+
+
+def enrich_data(records: t.List[Record]) -> t.List[Record]:
+    for record in records:
+        logging.info(f"Got email: {record.value['payload']['email']}")
+
+        enrichment = enrich_user_email(record.value['payload']['email'])
+
+        record.value['payload']['full_name'] = enrichment.full_name
+        record.value['payload']['company'] = enrichment.company
+        record.value['payload']['location'] = enrichment.location
+        record.value['payload']['role'] = enrichment.role
+        record.value['payload']['seniority'] = enrichment.seniority
+
+    return records
+
+
+class App:
+    @staticmethod
+    async def run(turbine: Runtime):
+
+        logging.basicConfig(level=logging.INFO)
+
+        try:
+            # Get remote resource
+            resource = await turbine.resources("demopg")
+
+            # Read from remote resource
+            records = await resource.records("user_activity")
+
+            # Makes environment variable available to the data-application
+            turbine.register_secrets("CLEARBIT_API_KEY")
+
+            # Deploy function with source as input
+            enriched = await turbine.process(records, enrich_data)
+
+            # Write results out
+            await resource.write(enriched, "user_activity_enriched")
+
+        except ChildProcessError as cpe:
+            print(cpe)
+        except Exception as e:
+            print(e)
